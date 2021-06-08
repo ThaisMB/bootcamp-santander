ee4b35f0cb8091c5245f8e885f59d29c24a60d96
Thaís Barbosa <thaismb26@gmail.com>
2021-06-08 12:26:55 -0300

    criacao de repositorio com o codigo do bootcamp

---
+++ src/main/java/com/project/bootcamp/util/MessageUtils.java
@@ -0,0 +1,6 @@
+package com.project.bootcamp.util;
+
+public abstract class MessageUtils {
+    public static final String STOCK_ALREADY_EXISTS = "Stocks already exists in the database";
+    public static final String NO_RECORDS_FOUNDS = "No records found";
+}
